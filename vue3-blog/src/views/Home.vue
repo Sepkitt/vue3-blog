8112387f42fb9dc72c17e273c6fda4595a9721d2
Jermaine Sepkitt <99402948+Sepkitt@users.noreply.github.com>
2022-05-19 13:52:04 +0200

    Initiliazed project

---
+++ vue3-blog/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Home">
+        <h1>Home</h1>
+       <p ref="p"> My name is {{name}} and I am {{age}} years old</p>
+    </div>
+
+    <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    name: 'Home',
+    setup () {
+
+        const p = ref(null)
+
+        let name = 'Mario'
+        let age = 30
+        
+        const handleClick = (() => {
+            console.log(p, p.value)
+            p.value.classList.add('test')
+            p.value.textContent = "Hello, Ninjas"
+        })
+        
+
+        return { name, age, handleClick, p}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
